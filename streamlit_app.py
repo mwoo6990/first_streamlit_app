60739219fe73da1d91508582b356217f7327a0ec
mwoo6990 <145765082+mwoo6990@users.noreply.github.com>
2023-09-22 11:20:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,11 @@ def get_fruityvice_data(this_fruit_choide):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * FROM fruit_load_list")
+        return my_cur.fetchall()
+
 streamlit.header("Fruityvice Fruit Advice!")
 
 try:
@@ -48,12 +53,8 @@ except URLError as e:
 
 
 
-
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT* FROM fruit_load_list")
-my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(getfruit_load_list())
 
 fruit_to_add = streamlit.text_input("What fruit would you like to add?", "jackfruit")
 my_cur.execute(f"insert into fruit_load_list values('{fruit_to_add}')")
